0886b7c7dde8844c7521a4fa5180f0875ee207b0
mindtable <mindtable@yandex-team.ru>
2023-09-27 10:04:54 +0300

    issue-4: implement storage-component

--- storage-service/storage-component/build.gradle.kts
+++ storage-service/storage-component/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.getByName<Test>("test") {
