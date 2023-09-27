2fef187c197075d0c7c0e8e305de9379ef1f066b
mindtable <mindtable@yandex-team.ru>
2023-09-27 09:25:55 +0300

    issue-2: add CLI

--- client/src/main/kotlin/ru/itmo/client/command/RootCommand.kt
+++ client/src/main/kotlin/ru/itmo/client/command/RootCommand.kt
@@ -3,12 +3,17 @@ package ru.itmo.client.command
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
 import picocli.CommandLine.Command
+import ru.itmo.client.command.get.GetCommand
+import ru.itmo.client.command.set.SetCommmand
 import java.util.concurrent.Callable
 
 @Service
 @Command(
     name = "root",
-    subcommands = [GetCommand::class],
+    subcommands = [
+        GetCommand::class,
+        SetCommmand::class,
+    ],
 )
 class RootCommand : Callable<Int> {
     private val log = KotlinLogging.logger { }
