2f4ec6f315b961c871035bb3d29ea4efb4972146
Karim Khasan <55288330+Kkkkkk58@users.noreply.github.com>
2023-09-29 16:09:07 +0300

    issue-6: add autoconfigs

--- storage-service/storage-component/src/main/kotlin/ru/itmo/storage/storage/config/FileSystemKeyValueRepositoryConfiguration.kt
+++ storage-service/storage-component/src/main/kotlin/ru/itmo/storage/storage/config/FileSystemKeyValueRepositoryConfiguration.kt
@@ -1,6 +1,9 @@
 package ru.itmo.storage.storage.config
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import ru.itmo.storage.storage.local.FileSystemKeyValueRepository
@@ -12,4 +15,12 @@ import ru.itmo.storage.storage.local.properties.FileSystemRepositoryProperties
     FileSystemKeyValueRepository::class,
 )
 @EnableConfigurationProperties(FileSystemRepositoryProperties::class)
-class FileSystemKeyValueRepositoryConfiguration
+class FileSystemKeyValueRepositoryConfiguration(@Autowired private val repositoryProperties: FileSystemRepositoryProperties)
+{
+    @Bean
+    @ConditionalOnProperty(prefix = "storage.component.filesystem.type", havingValue = "local" , matchIfMissing = false)
+    fun localFileSystemKeyValueRepository(): FileSystemKeyValueRepository {
+        return FileSystemKeyValueRepository(repositoryProperties)
+    }
+
+}
