48cf0a4942c7271662ea3c373ea9eeb092283cb2
Karim Khasan <55288330+Kkkkkk58@users.noreply.github.com>
2023-12-08 22:09:20 +0300

    issue-56: fix files ordering on sstable loading

--- storage-service/storage-component/src/main/kotlin/ru/itmo/storage/storage/lsm/sstable/LocalSSTableLoader.kt
+++ storage-service/storage-component/src/main/kotlin/ru/itmo/storage/storage/lsm/sstable/LocalSSTableLoader.kt
@@ -8,8 +8,6 @@ import ru.itmo.storage.storage.lsm.properties.LsmRepositoryFlushProperties
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.attribute.BasicFileAttributes
-import java.nio.file.attribute.FileTime
 import kotlin.io.path.isDirectory
 
 @Service
@@ -31,11 +29,6 @@ class LocalSSTableLoader(
     }
 
     private fun compareByCreationTimeDesc(path1: Path, path2: Path): Int {
-        val creationTime1 = getCreationTime(path1)
-        val creationTime2 = getCreationTime(path2)
-        return creationTime2.compareTo(creationTime1)
+        return path1.compareTo(path2)
     }
-
-    private fun getCreationTime(path: Path): FileTime =
-        Files.readAttributes(path, BasicFileAttributes::class.java).creationTime()
 }
